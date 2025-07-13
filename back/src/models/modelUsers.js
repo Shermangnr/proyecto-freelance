9c6790d11a958051525235d91d2030526c0694c4
German Bermudez <shermangnr@gmail.com>
2025-07-13 18:45:23 -0500

    Modificacion de archivo modelUsers para mejor lectura

--- back/src/models/modelUsers.js
+++ back/src/models/modelUsers.js
@@ -1,22 +1,66 @@
 import { Schema, model } from 'mongoose';
+import { type } from 'os';
 
 const schemaUser = new Schema({
-    name: {
+    nombre: {
         type: String,
         required: true,
     },
-    email: {
+    correo: {
         type: String, 
         required: true,
         match: [/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/, "email invalid."]
     },
-    password: {
+    contrasena: {
         type: String,
         required: true, 
-        minlength: [7, "The password must have least seven characters"], 
+        minlength: [3, "The password must have least seven characters"], 
         trim: true, // Elimina los espacios en blanco del inicio y final
-        match:[/^(?=.*[!@#$%^&*()_\-+={}[\]|\\:;"'<>,.?/~`])\S+$/, "Password invalid. The password must have a special character."] // Validacion que tenga lo indicado
+        // match:[/^(?=.*[!@#$%^&*()_\-+={}[\]|\\:;"'<>,.?/~`])\S+$/, "Password invalid. The password must have a special character."] // Validacion que tenga lo indicado
     },
+    imagenPerfil: {
+        type: String,
+        default: ''
+    },
+    bio: {
+        type: String,
+        default: ''
+    },
+    ubicacion: {
+        type: String,
+        default: ''
+    },
+    habilidades: {
+        type: [String],
+        default: []
+    },
+    profesion: {
+        type: String,
+        default: ''
+    },
+    precioServicioHora: {
+        type: Number,
+        default: 0
+    },
+    linksRedes: {
+        linkedin: {
+            type: String,
+            default: ''
+        },
+        github: {
+            type: String,
+            default: ''
+        },
+        portafolio: {
+            type: String,
+            default: ''
+        }
+    },
+    fechaCreacion: {
+        type: Date,
+        default: Date.now
+    }
+
 });
 
 export default model ("User", schemaUser);
