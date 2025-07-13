584cb22c5fc632da9d61db89992a457d7cb15677
German Bermudez <shermangnr@gmail.com>
2025-07-13 18:52:20 -0500

    Creacion de router para el Login

---
+++ back/src/routers/routerLogin.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import controllerLogin from "../controllers/controllerLogin.js";
+
+const routerLogin = Router();
+
+routerLogin.post('/', controllerLogin.iniciarSesion);
+routerLogin.get('/token/:token', controllerLogin.validarToken);
+
+export default routerLogin;
