c056dd576c8f3afaff9d85ade871544fea1cc8ef
German Bermudez <shermangnr@gmail.com>
2025-07-13 18:46:22 -0500

    Importacion de nuevos enrutadores de servicios e inclusion de funciones de esos servicios anadidos

--- back/src/servidor.js
+++ back/src/servidor.js
@@ -3,11 +3,16 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 import routerUsers from './routers/routerUsers.js';
+import routerServices from "./routers/routerServices.js";
+import routerLogin from "./routers/routerLogin.js";
 
 const servidor = express();
 servidor.use(cors());
 servidor.use(morgan("dev"));
 servidor.use(express.json());
 servidor.use('/usuarios', routerUsers);
+servidor.use('/inicio-sesion', routerLogin);//login
+servidor.use('/servicios', routerServices);
+servidor.use('/images', express.static(path.resolve('images')));
 
 export default servidor;
